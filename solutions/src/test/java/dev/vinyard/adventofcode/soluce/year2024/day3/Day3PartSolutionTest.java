5ab634ef5650961f8f747a2d79be08dc1124d28e
Viinyard <viinyard@users.noreply.github.com>
2024-12-03 10:21:41 +0000

    Next solution

---
+++ solutions/src/test/java/dev/vinyard/adventofcode/soluce/year2024/day3/Day3PartSolutionTest.java
@@ -0,0 +1,23 @@
+package dev.vinyard.adventofcode.soluce.year2024.day3;
+
+import dev.vinyard.adventofcode.common.BaseTest;
+import dev.vinyard.aoc.plugins.solution.api.Solution;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+public class Day3PartSolutionTest extends BaseTest<Object> {
+
+    @Override
+    public Solution<Object> getSolution() {
+        return new Day3PartSolution();
+    }
+
+    @Override
+    public Stream<Arguments> testSolution() {
+        return Stream.of(
+            Arguments.of("soluce/year2024/day3/part/test.txt", null, null, null),
+            Arguments.of("soluce/year2024/day3/input.txt", null, null, null)
+        );
+    }
+}
