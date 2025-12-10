b430f57a621e8531dd61ee5abb5f62a47bad0229
aleval <antoine.leval@capgemini.com>
2025-12-10 14:28:16 +0100

    [Year 2025 Day 10 Part 1] ok

--- solutions/src/main/antlr4/dev/vinyard/adventofcode/soluce/year2025/day10/Solution.g4
+++ solutions/src/main/antlr4/dev/vinyard/adventofcode/soluce/year2025/day10/Solution.g4
@@ -9,8 +9,136 @@ options {
 }
 
 root returns [ASD.Root out]
-    :
+		@init {
+			List<ASD.Machine> machines = new ArrayList<>();
+		}
+    : (machine { machines.add($machine.out); })+ EOF {
+			$out = new ASD.Root(machines);
+    }
     ;
+
+machine returns [ASD.Machine out]
+		: indicator_light button_wiring_schematics joltage_requirements {
+				$out = new ASD.Machine($indicator_light.out, $button_wiring_schematics.out, $joltage_requirements.out);
+		}
+		;
+
+button_wiring_schematics returns [List<ASD.ButtonWiringSchematic> out]
+		@init {
+				$out = new ArrayList<>();
+		}
+		: (button_wiring_schematic {
+				$out.add($button_wiring_schematic.out);
+		})+
+		;
+
+button_wiring_schematic returns [ASD.ButtonWiringSchematic out]
+		: OPEN_PAREN buttons CLOSE_PAREN {
+				$out = new ASD.ButtonWiringSchematic($buttons.out);
+		}
+		;
+
+joltage_requirements returns [ASD.JoltageRequirements out]
+		: OPEN_CURLY joltages CLOSE_CURLY {
+				$out = new ASD.JoltageRequirements($joltages.out);
+		}
+		;
+
+joltages returns [List<Integer> out]
+		@init {
+				$out = new ArrayList<>();
+		}
+		: joltage {
+				$out.add($joltage.out);
+		} (COMMA joltage {
+				$out.add($joltage.out);
+		})*
+		;
+
+joltage returns [Integer out]
+		: INT {
+				$out = Integer.parseInt($INT.text);
+		}
+		;
+
+buttons returns [List<ASD.Button> out]
+		@init {
+				$out = new ArrayList<>();
+		}
+		: button {
+				$out.add($button.out);
+		} (COMMA button {
+				$out.add($button.out);
+		})*
+		;
+
+button returns [ASD.Button out]
+		: INT {
+				$out = new ASD.Button(Integer.parseInt($INT.text));
+		}
+		;
+
+indicator_light returns [ASD.IndicatorLightDiagram out]
+		: OPEN_BRACKET light_states CLOSE_BRACKET {
+				$out = new ASD.IndicatorLightDiagram($light_states.out);
+		}
+		;
+
+light_states returns [List<ASD.LightState> out]
+		@init {
+				$out = new ArrayList<>();
+		}
+		: (light_state {
+				$out.add($light_state.out);
+			})*
+		;
+
+light_state returns [ASD.LightState out]
+		:
+		INDICATOR_LIGHT_OFF {
+				$out = ASD.LightState.OFF;
+		}
+		|
+		INDICATOR_LIGHT_ON {
+				$out = ASD.LightState.ON;
+		}
+		;
+
+OPEN_PAREN
+		: '('
+		;
+
+CLOSE_PAREN
+		: ')'
+		;
+
+COMMA
+		: ','
+		;
+
+OPEN_CURLY
+		: '{'
+		;
+
+CLOSE_CURLY
+		: '}'
+		;
+
+OPEN_BRACKET
+		: '['
+		;
+
+CLOSE_BRACKET
+		: ']'
+		;
+
+INDICATOR_LIGHT_OFF
+		: '.'
+		;
+
+INDICATOR_LIGHT_ON
+		: '#'
+		;
 
 INT
     // integer part forbis leading 0s (e.g. `01`)
--- solutions/src/main/java/dev/vinyard/adventofcode/soluce/year2025/day10/ASD.java
+++ solutions/src/main/java/dev/vinyard/adventofcode/soluce/year2025/day10/ASD.java
@@ -1,9 +1,123 @@
 package dev.vinyard.adventofcode.soluce.year2025.day10;
 
+import lombok.Getter;
+import org.jgrapht.Graph;
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.builder.GraphTypeBuilder;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public class ASD {
 
     public static class Root {
 
+        private final List<Machine> machines;
+
+        public Root(List<Machine> machines) {
+            this.machines = machines;
+        }
+
+        public long solution1() {
+            return machines.stream().mapToLong(Machine::solve).sum();
+        }
+    }
+
+    public static class Machine {
+        private final List<ButtonWiringSchematic> buttonWiringSchematics;
+        private final JoltageRequirements joltageRequirements;
+        private final IndicatorLightDiagram indicateurLightDiagram;
+
+        public Machine(IndicatorLightDiagram indicateurLightDiagram, List<ButtonWiringSchematic> buttonWiringSchematics, JoltageRequirements joltageRequirements) {
+            this.indicateurLightDiagram = indicateurLightDiagram;
+            this.buttonWiringSchematics = buttonWiringSchematics;
+            this.joltageRequirements = joltageRequirements;
+        }
+
+        private Graph<Integer, DefaultWeightedEdge> createEmptyGraph() {
+            return GraphTypeBuilder.<Integer, DefaultWeightedEdge>undirected()
+                    .allowingMultipleEdges(false)
+                    .allowingSelfLoops(false)
+                    .edgeClass(DefaultWeightedEdge.class)
+                    .weighted(true)
+                    .buildGraph();
+        }
+
+        public long solve() {
+            Graph<Integer, DefaultWeightedEdge> graph = createEmptyGraph();
+
+            indicateurLightDiagram.getAllSubDiagrams().forEach(graph::addVertex);
+
+            graph.vertexSet().forEach(startVertex -> {
+                buttonWiringSchematics.stream().map(ButtonWiringSchematic::getWiringState).forEach(schematic -> {
+                    graph.addEdge(startVertex, startVertex ^ schematic);
+                });
+            });
+
+            ShortestPathAlgorithm<Integer, DefaultWeightedEdge> dijkstraAlg = new DijkstraShortestPath<>(graph);
+
+            return (long) dijkstraAlg.getPathWeight(0, indicateurLightDiagram.getState());
+        }
+    }
+
+    public static class Button {
+
+        private final int id;
+
+        public Button(int id) {
+            this.id = id;
+        }
+
+        public int getState() {
+            return 1 << id;
+        }
+    }
+
+    public static class ButtonWiringSchematic {
+        private final List<Button> buttons;
+
+        @Getter
+        private final int wiringState;
+
+        public ButtonWiringSchematic(List<Button> buttons) {
+            this.buttons = buttons;
+            this.wiringState = buttons.stream().map(Button::getState).reduce(0, (a, b) -> a | b);
+        }
+    }
+
+    public static class JoltageRequirements {
+
+        private final List<Integer> joltages;
+
+        public JoltageRequirements(List<Integer> joltages) {
+            this.joltages = joltages;
+        }
+    }
+
+    public static class IndicatorLightDiagram {
+        private final List<LightState> lightStates;
+
+        public IndicatorLightDiagram(List<LightState> lightStates) {
+            this.lightStates = lightStates;
+        }
+
+        public Integer getState() {
+            return Stream.iterate(0, i -> i + 1).limit(lightStates.size())
+                    .filter(i -> Objects.equals(lightStates.get(i), LightState.ON))
+                    .reduce(0, (a, b) -> a | (1 << b));
+        }
+
+        public List<Integer> getAllSubDiagrams() {
+            return Stream.iterate(0, i -> i + 1).limit(1L << lightStates.size()).toList();
+        }
+    }
+
+    public enum LightState {
+        OFF,
+        ON
     }
 
 }
